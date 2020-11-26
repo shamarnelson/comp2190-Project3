79a50c0fa78aa12169de761f1e099d9893296035
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2020-11-25 23:41:54 -0500

    Initial Files

---
+++ Student_ID/scripts/VoteLogDB.php
@@ -0,0 +1,12 @@
+<?php
+if( isset( $_POST['submit_form'] ) )
+{
+    $cID = $_POST['cID'];
+    $constID = $_POST['constID'];
+    $pollDivID = $_POST['pollDivID'];
+    $pollStn = $_POST['pollStn'];
+    $votesforCand1 = $_POST['votesforCand1'];
+    $votesforCand2 = $_POST['votesforCand2'];
+    $rejectedBal = $_POST['rejectedBal'];
+    $totalNumVote = $_POST['totalNumVote'];
+    $hidden = $_POST['hidden'];
