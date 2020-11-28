bc47a5d68d40a6f6c32516f202780e873e277c38
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2020-11-27 22:22:04 -0500

    Initial Files

--- Student_ID/scripts/phpmysqlconnect.php
+++ Student_ID/scripts/phpmysqlconnect.php
@@ -1,28 +1,35 @@
 <?php
 
 require_once 'dbconfig.php';
+require_once 'p1.html'
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     echo "Connected to $dbname at $host successfully.";
     $conn = null;
+
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
-if( isset( $_POST['submit_form'] ) )
+$insertData= "INSERT INTO StationVotes 
+(id, constituency_id,clerk_id,poll_division_id,polling_station_code,candidate1Votes,candidate2Votes
+rejectedVotes,totalVotes) VALUES('$id','$constituency_id','$clerk_id','$poll_division_id','$polling_station_code','$candidate1Votes','$candidate2Votes',
+
+'$rejectedVotes','$totalVotes')";
+
+if( isset( $_POST['username']&& isset($_POST['password']) ) )
 {
-    $cID = $_POST['cID'];
-    $constID = $_POST['constID'];
-    $pollDivID = $_POST['pollDivID'];
-    $pollStn = $_POST['pollStn'];
-    $votesforCand1 = $_POST['votesforCand1'];
-    $votesforCand2 = $_POST['votesforCand2'];
-    $rejectedBal = $_POST['rejectedBal'];
-    $totalNumVote = $_POST['totalNumVote'];
-    $hiddenField = $_POST['hiddenField'];
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
 }
+$stmt = $conn ->query("SELECT * FROM StationVotes");
+$results = $stmt ->fetchALL(PDO:: FETCH_ASSOC);
+
+$htmlTable .= <p1 class="html"></p1>
+echo $htmlTable;
 
 
 
 
-
+?>
